f9e5e44433bc49fa2752bc1329418997fb2f39b7
S Fujimoto <unicoshun@gmail.com>
2018-09-16 15:55:14 +0900

    add task_answer

---
+++ for_sep_22/task_answers/task4_answer.py
@@ -0,0 +1,21 @@
+# Task4: 条件をつけよう
+
+a = input("最初の数字はなんですか？")
+b = input("次の数字はなんですか？")
+a = int(a)
+b = int(b)
+
+# ユーザーに足し算か引き算かたずねよう
+# >> userinput = input("足し算の場合は - a / 引き算の場合は - m と入力")
+userinput = input("足し算の場合は - a / 引き算の場合は - m と入力")
+
+# 足し算の場合と引き算の場合で計算を変えよう
+# >> if userinput == 'a':
+# >>    print("足し算の結果は" + str(a + b) + "です。")
+# >> elif userinput == 'm':
+# >>    print("引き算の結果は" + str(a - b) + "です。")
+
+if userinput == 'a':
+    print("足し算の結果は" + str(a + b) + "です。")
+elif userinput == 'm':
+    print("引き算の結果は" + str(a - b) + "です。")
